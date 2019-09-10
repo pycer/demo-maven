d07e3fcf8123d186ea836bdb1239dfcfdaf42aa9
pycer <pycer010@gmail.com>
2019-09-10 09:21:16 +0800

    demo init

---
+++ src/test/java/com/sangfor/maven/demomaven/DemoMavenApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sangfor.maven.demomaven;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoMavenApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
